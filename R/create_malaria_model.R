0d0bf5b1ec510078a39a01c97dc90ccead8791d0
ChaokunHong <chaokunhong@gmail.com>
2024-05-01 10:41:30 +0800

    Fixed initialization of mosquito population variables

--- R/create_malaria_model.R
+++ R/create_malaria_model.R
@@ -20,18 +20,33 @@ NULL
 #' @param rA Rate of recovery from asymptomatic to susceptible status
 #' @param rTs Rate of recovery from treatment to susceptible status
 #' @param rTR Rate of recovery from treatment to resistant status
+#' @param Sv0 Initial susceptible mosquito population
+#' @param Ev_s0 Initial exposed mosquito population (susceptible strain)
+#' @param Iv_s0 Initial infectious mosquito population (susceptible strain)
+#' @param Ev_r0 Initial exposed mosquito population (resistant strain)
+#' @param Iv_r0 Initial infectious mosquito population (resistant strain)
+#' @param e Egg laying rate of mosquitoes
+#' @param mu Mortality rate of mosquitoes
+#' @param n Delay in days for mosquito development
+#' @param Lambda_v_s Infection rate for susceptible strain
+#' @param Lambda_v_r Infection rate for resistant strain
 #' @return An object of class `odin_model`.
 #' @export
 #' @examples
 #' model <- create_malaria_model(S0 = 1000, Ds0 = 100, As0 = 50, Ts0 = 25, DR0 = 10, AR0 = 5, TR0 = 2,
 #'                               Lambda_s = 0.3, Lambda_R = 0.1, Phi = 0.7, fT = 0.2,
-#'                               rD = 0.05, rA = 0.03, rTs = 0.02, rTR = 0.01)
+#'                               rD = 0.05, rA = 0.03, rTs = 0.02, rTR = 0.01,
+#'                               Sv0 = 500, Ev_s0 = 0, Iv_s0 = 0, Ev_r0 = 0, Iv_r0 = 0,
+#'                               e = 100, mu = 0.1, n = 10, Lambda_v_s = 0.005, Lambda_v_r = 0.003)
 #' results <- model$run(0:50)
 #' print(results)
-create_malaria_model <- function(S0, Ds0, As0, Ts0, DR0, AR0, TR0, Lambda_s, Lambda_R, Phi, fT, rD, rA, rTs, rTR) {
+create_malaria_model <- function(S0, Ds0, As0, Ts0, DR0, AR0, TR0, Lambda_s, Lambda_R, Phi, fT, rD, rA, rTs, rTR,
+                                 Sv0, Ev_s0, Iv_s0, Ev_r0, Iv_r0, e, mu, n, Lambda_v_s, Lambda_v_r) {
   model_code <- readLines(system.file("odin/model.R", package = "AMRSpreadModel"))
   model <- odin::odin(model_code)
   model$new(user = list(S0 = S0, Ds0 = Ds0, As0 = As0, Ts0 = Ts0, DR0 = DR0, AR0 = AR0, TR0 = TR0,
                         Lambda_s = Lambda_s, Lambda_R = Lambda_R, Phi = Phi, fT = fT,
-                        rD = rD, rA = rA, rTs = rTs, rTR = rTR))
+                        rD = rD, rA = rA, rTs = rTs, rTR = rTR,
+                        Sv0 = Sv0, Ev_s0 = Ev_s0, Iv_s0 = Iv_s0, Ev_r0 = Ev_r0, Iv_r0 = Iv_r0,
+                        e = e, mu = mu, n = n, Lambda_v_s = Lambda_v_s, Lambda_v_r = Lambda_v_r))
 }
--- inst/odin/model.R
+++ inst/odin/model.R
@@ -1,15 +1,15 @@
-## Equations
-deriv(S) <- - S * Lambda_s * (Phi * fT + Phi * (1-fT) + (1-Phi)) -
-  S * Lambda_R * (Phi * fT + Phi * (1-fT) + (1-Phi)) +
+## Human Equations
+deriv(S) <- -S * Lambda_s * (Phi * fT + Phi * (1 - fT) + (1 - Phi)) -
+  S * Lambda_R * (Phi * fT + Phi * (1 - fT) + (1 - Phi)) +
   Ts * rTs + As * rA + AR * rA + TR * rTR
 
-deriv(Ds) <- S * Lambda_s * Phi * (1-fT) +
-  Lambda_s * As * Phi * (1-fT) -
+deriv(Ds) <- S * Lambda_s * Phi * (1 - fT) +
+  Lambda_s * As * Phi * (1 - fT) -
   Ds * rD
 
-deriv(As) <- S * Lambda_s * (1-Phi) +
+deriv(As) <- S * Lambda_s * (1 - Phi) +
   Ds * rD -
-  Lambda_s * As * Phi * (1-fT) -
+  Lambda_s * As * Phi * (1 - fT) -
   Lambda_s * As * Phi * fT -
   As * rA
 
@@ -17,19 +17,34 @@ deriv(Ts) <- S * Lambda_s * Phi * fT +
   Lambda_s * As * Phi * fT -
   Ts * rTs
 
-deriv(DR) <- S * Lambda_R * Phi * (1-fT) +
-  Lambda_R * AR * Phi * (1-fT) -
+deriv(DR) <- S * Lambda_R * Phi * (1 - fT) +
+  Lambda_R * AR * Phi * (1 - fT) -
   DR * rD
 
-deriv(AR) <- S * Lambda_R * (1-Phi) +
+deriv(AR) <- S * Lambda_R * (1 - Phi) +
   DR * rD -
-  Lambda_R * AR * Phi * (1-fT) -
+  Lambda_R * AR * Phi * (1 - fT) -
   Lambda_R * AR * Phi * fT -
   AR * rA
 
 deriv(TR) <- S * Lambda_R * Phi * fT +
   Lambda_R * AR * Phi * fT -
   TR * rTR
+
+## Mosquito Equations
+deriv(Sv) <- e - (Lambda_v_s + Lambda_v_r) * Sv - mu * Sv
+
+delayed_Lambda_v_s_Sv_raw <- delay(Lambda_v_s * Sv, n)
+delayed_Lambda_v_s_Sv <- delayed_Lambda_v_s_Sv_raw * exp(-mu * n)
+
+deriv(Ev_s) <- Lambda_v_s * Sv - delayed_Lambda_v_s_Sv - mu * Ev_s
+deriv(Iv_s) <- delayed_Lambda_v_s_Sv - mu * Iv_s
+
+delayed_Lambda_v_r_Sv_raw <- delay(Lambda_v_r * Sv, n)
+delayed_Lambda_v_r_Sv <- delayed_Lambda_v_r_Sv_raw * exp(-mu * n)
+
+deriv(Ev_r) <- Lambda_v_r * Sv - delayed_Lambda_v_r_Sv - mu * Ev_r
+deriv(Iv_r) <- delayed_Lambda_v_r_Sv - mu * Iv_r
 
 ## Initial conditions
 initial(S) <- S0
@@ -39,6 +54,11 @@ initial(Ts) <- Ts0
 initial(DR) <- DR0
 initial(AR) <- AR0
 initial(TR) <- TR0
+initial(Sv) <- Sv0
+initial(Ev_s) <- Ev_s0
+initial(Iv_s) <- Iv_s0
+initial(Ev_r) <- Ev_r0
+initial(Iv_r) <- Iv_r0
 
 ## User-defined parameters
 S0 <- user()
@@ -56,3 +76,13 @@ rD <- user()
 rA <- user()
 rTs <- user()
 rTR <- user()
+Sv0 <- user()
+Ev_s0 <- user()
+Iv_s0 <- user()
+Ev_r0 <- user()
+Iv_r0 <- user()
+e <- user()
+mu <- user()
+n <- user()
+Lambda_v_s <- user()
+Lambda_v_r <- user()
